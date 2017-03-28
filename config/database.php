99dcb97684eed1b7e84ed14968a37de38562e550
felixbvf <felixbvf@gmail.com>
2017-03-28 19:46:44 -0400

    Created other Database foro_test in postgresql

--- config/database.php
+++ config/database.php
@@ -33,18 +33,13 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
 
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'foro'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -57,10 +52,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'port' => env('DB_PORT_PG', '5433'),
+            'database' => env('DB_DATABASE_PG', 'foro_test'),
+            'username' => env('DB_USERNAME_PG', 'postgres'),
+            'password' => env('DB_PASSWORD_PG', '123'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
