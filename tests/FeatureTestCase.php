554a7203f48e90d9cf2d5c3136c95844e91696f6
felixbvf <felixbvf@gmail.com>
2017-03-30 18:10:50 -0400

    bug fixes test

--- tests/FeatureTestCase.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-class FeatureTestCase extends TestCase
-{   use DatabaseTransactions;
-
-}
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,21 +3,18 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-//use Illuminate\Foundation\Testing\DatabaseMigrations;
-//use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class ExampleTest extends FeatureTestCase
+class ExampleTest extends TestCase
 {
-    //use DatabaseTransactions;
-
-    public function test_basic_example()
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
     {
         $this->assertTrue(true);
-        /*$user = factory(\Foro\User::class)->create([
-            'name' => 'Felix Balderrama',
-        ]);
-
-        $this->actingAs($user,'api')->visit('api/user')->see('Felix Balderrama');*/
     }
 }
